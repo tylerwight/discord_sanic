c03b34b11350862372f16a6199f51a5c85d396d0
Tyler <tylermwight@gmail.com>
2022-09-16 17:41:40 +0000

    separate bot and sanic (async not worth)

---
+++ bot_test.py
@@ -0,0 +1,29 @@
+from sanic import Sanic
+from sanic.response import json, html, text
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+import asyncio
+
+#DISCORD setup
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="^",intents=intents)
+
+
+
+#=========
+#BOT COMMANDS
+#=========
+
+@bot.command(brief="test ping pong command")
+async def ping(ctx):
+    await ctx.send("bitch")
+
+
+bot.run(TOKEN)
+
+
+
--- sanic_test.py
+++ sanic_test.py
@@ -1,12 +1,24 @@
 from sanic import Sanic
 from sanic.response import json, html, text
 import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+import asyncio
 
+
+#SANIC setup
 app = Sanic(__name__)
-
+#===========
+#SANIC
+#==========
 @app.route('/')
-def index(request):
+async def index(request):
     template = open(os.getcwd() + "/templates/index.html")
     return html(template.read())
 
+
+
+
 app.run(host='0.0.0.0', port=8081)
+
